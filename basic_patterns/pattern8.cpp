9a8f49aab6d42640be2223c35b2b82b4c32a7bc9
eashaan-rao <rao.eashaan@gmail.com>
2025-03-19 15:46:33 +0530

    Solved Pattern7 and Pattern8

---
+++ basic_patterns/pattern8.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+/*
+pattern 7:
+Ninja was very fond of patterns. For a given integer ‘N’, he wants to make the Reverse N-Star Triangle.
+Input N=3
+*****
+ ***
+  *
+*/
+
+void nStarTriangle(int n) {
+    int k=1;
+    for(int i=n; i>=1; i--) {
+        cout<<setw(k);
+        int m = i*2 - 1;
+        for(int j=0; j<m; j++) {
+            cout<<"*";
+        }
+        ++k;
+        cout<<endl;
+    }
+}
+
+int main() {
+
+    nStarTriangle(3);
+
+    return 0;
+}
